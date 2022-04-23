7751faba34a252dc91a1e0d776a7238a594579dc
Aleksey Smychenko <93550750+alfrntlower@users.noreply.github.com>
2022-04-23 23:50:03 +0300

    Task 2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const CHANGE_COLOR_DELAY = 1000;
 
 let intervalId = null;
 
-// startBtn.setAttribute('disabled', 'disabled')
+// startBtn.setAttribute('disabled', 'disabled');
 
 startBtn.addEventListener('click', onStartBtnClick);
 stopBtn.addEventListener('click',onStopBtnClick);
@@ -14,7 +14,7 @@ stopBtn.addEventListener('click',onStopBtnClick);
 function onStartBtnClick() {
     console.log("START");
     intervalId = setInterval(() => {
-        console.log(getRandomHexColor());
+        //console.log(getRandomHexColor());
         document.body.style.background = getRandomHexColor();
     }, CHANGE_COLOR_DELAY);
 
@@ -30,7 +30,6 @@ function onStopBtnClick() {
     changeBtnStatus(true, false);
 
 }
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
---
+++ src/js/02-timer.js
@@ -0,0 +1,107 @@
+// Описан в документации
+import flatpickr from "flatpickr";
+// Дополнительный импорт стилей
+import "flatpickr/dist/flatpickr.min.css";
+
+const startBtn = document.querySelector('button[data-start]');
+const daysValue = document.querySelector('span[data-days]');
+const hoursValue = document.querySelector('span[data-hours]');
+const minutesValue = document.querySelector('span[data-minutes]');
+const secondsValue = document.querySelector('span[data-seconds]');
+const dateInput = document.querySelector("#datetime-picker");
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+const dateNow = new Date();
+const date = flatpickr('#datetime-picker', options);
+
+dateInput.addEventListener('change', checkInputDate);
+startBtn.addEventListener('click', onStartBtnClick);
+
+
+function onStartBtnClick(evt) {
+
+    changeBtnStatus(true);
+
+    let dateMs = date.selectedDates[0].getTime();
+
+    let dateNowMs = dateNow.getTime();
+
+    console.log('MS :',dateMs);
+
+    console.log('NOW MS:',dateNowMs);
+
+    // const deltaMs = dateMs - dateNowMs;
+    
+    // const afterConvert = convertMs(timerMs);
+
+    // console.log(afterConvert);
+    startTimer(dateMs, dateNowMs);
+
+    
+
+}
+
+function startTimer(dateMs,dateNowMs) {
+    setInterval(() => {
+        let deltaMs = dateMs - dateNowMs;
+        console.log(convertMs(deltaMs));
+        updateClockface(convertMs(deltaMs));
+        }, 1000);
+
+}
+
+function checkInputDate() {
+
+    let dateMs = date.selectedDates[0].getTime();
+    let dateNowMs = dateNow.getTime();
+
+    if (dateMs < dateNowMs) {
+        window.alert("Please choose a date in the future");
+        changeBtnStatus(true);
+    } else {
+        changeBtnStatus(false);
+    }
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = pad(Math.floor(ms / day));
+  // Remaining hours
+  const hours = pad(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = pad(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = pad(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+function pad(value) {
+    return String(value).padStart(2, '0');
+}
+  
+const changeBtnStatus = (add) => {
+    startBtn.disabled = add;
+};
+
+function updateClockface({ days, hours, minutes, seconds }) {
+    daysValue.textContent = days;
+    hoursValue.textContent = hours;
+    minutesValue.textContent = minutes;
+    secondsValue.textContent = seconds; 
+}
